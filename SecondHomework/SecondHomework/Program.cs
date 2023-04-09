bd021f49c619bd453865320f2495ed6d8e1ab232
Negadyai <a.enotov21@mail.ru>
2023-04-09 21:46:35 +0300

    Base

---
+++ SecondHomework/SecondHomework/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Homework
+{
+    public class SecondHomework
+    {
+        static void Main(string[] args)
+        {
+            int catCounter = 3;
+            float literCounter = 12.5f;
+            long nationalBudget = 4000000000000;
+            byte damage = 15;
+            short moveSpeed = 4;
+            double bulletSpeed = 7.3;
+
+            string name = "Ivan";
+            char classDesignation = 'A';
+
+            bool isActive = true;
+
+            object surname = "Ivanov";
+
+        }
+    }
+}
