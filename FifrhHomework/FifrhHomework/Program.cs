463b261b694199b631d71cbe33e882e5dfe7d9ea
Negadyai <a.enotov21@mail.ru>
2023-04-11 18:18:54 +0300

    FifthHomework

---
+++ FifrhHomework/FifrhHomework/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework
+{
+    public class FifrhHomeWork
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ivanov";
+            string surname = "Maxim";
+
+            Console.WriteLine($"{name} {surname}");
+
+            (name, surname) = (surname, name);
+
+            Console.WriteLine($"{name} {surname}");
+        }
+    }
+}
