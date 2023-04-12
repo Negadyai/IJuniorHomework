bc0f53719b990d096d1abaf60cc7b7561af1c93c
Negadyai <a.enotov21@mail.ru>
2023-04-12 16:35:27 +0300

    SixthHomework

---
+++ SixthHomework/SixthHomework/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Homework
+{
+    public class SixthHomework
+    {
+        static void Main(string[] args)
+        {
+            int maxAmountOfPhotos = 52;
+            int currentAmountOfPhotos = 0;
+            int amoutPhohotosInRow = 3;
+
+            int rowCounter = 0;
+
+            for (int i = 0; i < maxAmountOfPhotos; i++)
+            {
+                currentAmountOfPhotos++;
+                if (currentAmountOfPhotos == amoutPhohotosInRow)
+                {
+                    rowCounter++;
+                    currentAmountOfPhotos = 0;
+                }
+            }
+
+            Console.WriteLine(rowCounter);
+            Console.WriteLine($"Картинок сверхмеры: {maxAmountOfPhotos - rowCounter * amoutPhohotosInRow}");
+        }
+    }
+}
