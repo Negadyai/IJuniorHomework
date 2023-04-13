b71ee997110a0ebe1b621e972b0b70ea59df7cea
Negadyai <a.enotov21@mail.ru>
2023-04-13 16:05:27 +0300

    RevisionSixth

--- SixthHomework/SixthHomework/Program.cs
+++ SixthHomework/SixthHomework/Program.cs
@@ -4,24 +4,14 @@
     {
         static void Main(string[] args)
         {
-            int maxAmountOfPhotos = 52;
-            int currentAmountOfPhotos = 0;
+            int photos = 52;
             int amoutPhohotosInRow = 3;
 
-            int rowCounter = 0;
+            int rowAmount = photos / amoutPhohotosInRow;
+            int leftoverPhotos = photos % amoutPhohotosInRow;
 
-            for (int i = 0; i < maxAmountOfPhotos; i++)
-            {
-                currentAmountOfPhotos++;
-                if (currentAmountOfPhotos == amoutPhohotosInRow)
-                {
-                    rowCounter++;
-                    currentAmountOfPhotos = 0;
-                }
-            }
-
-            Console.WriteLine(rowCounter);
-            Console.WriteLine($"Картинок сверхмеры: {maxAmountOfPhotos - rowCounter * amoutPhohotosInRow}");
+            Console.WriteLine($"Количество рядов: {rowAmount}");
+            Console.WriteLine($"Картинок сверхмеры: {leftoverPhotos}");
         }
     }
 }
